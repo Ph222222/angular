43d0bf4e2d6c824147b397e488f76e4fe12b149c
ph222222 <philipbelder@gmail.com>
2016-07-23 22:03:25 +0300

    Another Commit

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var connect = require('connect');
-serveStatic = require('serve-static');
-
+var serveStatic = require('serve-static');
+var ipModule = require('./getIp');
 
 var app = connect();
 console.log ('hi');
+ipModule.getIp();
+var os = require('os');
+var ifaces = os.networkInterfaces();
+ 
 app.use(serveStatic("/www/node/angularjs"));
 app.listen(5000);
